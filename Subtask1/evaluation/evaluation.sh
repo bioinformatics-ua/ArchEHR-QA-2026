ed5dfa32564ab7bf1ac94e1966731937a1909b70
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2026-01-23 02:22:18 +0000

    Add SLURM script for running official evals

---
+++ Subtask1/evaluation/evaluation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=subtask1_evaluation
+#SBATCH --output=logs/eval%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+
+# --- Environment Setup ---
+echo "Job started on $(hostname)"
+echo "Job ID: $SLURM_JOB_ID"
+
+uv run python evaluation.py \
+    --submission_path ../outputs/predictions.json \
+    --key_path ../../data/dev/archehr-qa.xml \
+    --quickumls_path quickumls/ \
+    --out_file_path scores.json
