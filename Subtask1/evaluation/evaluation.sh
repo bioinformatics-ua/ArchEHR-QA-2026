2d392887ae95b2da3f8151bc265e46a15e860845
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2026-01-26 16:39:30 +0000

    Abstract model in evaluation script

--- Subtask1/evaluation/evaluation.sh
+++ Subtask1/evaluation/evaluation.sh
@@ -10,13 +10,14 @@
 # --- Environment Setup ---
 SIF_IMAGE="./builder.sif"
 UV_BIN=$(which uv)
+MODEL="google-gemini-3-pro-preview_prompt_3"
 
 # 1. Build dependencies (Fixes the C++ error)
 # singularity exec --nv "$SIF_IMAGE" "$UV_BIN" sync
 
 # 2. Run the code
 singularity exec --nv "$SIF_IMAGE" "$UV_BIN" run python evaluation.py \
-    --submission_path ../outputs/dev/google-gemini-3-pro-preview_prompt_3.json\
+    --submission_path ../outputs/dev/${MODEL}.json\
     --key_path ../../data/dev/archehr-qa.xml \
     --quickumls_path ../quickumls/final \
-    --out_file_path ../results/dev/google-gemini-3-pro-preview_prompt_3.json\
+    --out_file_path ../results/dev/${MODEL}.json
